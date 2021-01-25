d17a52a5766f10563952b2fa4df1592985d3bf5d
Anna <anna.stampferova@urbanstore.cz>
2021-01-25 20:25:25 +0100

    base edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
 import './App.scss';
-
-import { ShipList } from './components/ShipList/ShipList';
+import { Ships } from './pages/Ships/Ships';
+import { Rockets } from './pages/Rockets/Rockets';
 import { Nav } from './components/Nav/Nav';
 import { Footer } from './components/Footer/Footer';
 
 function App() {
     return (
-        <>
-            <header>
-                <Nav />
-            </header>
-            <main>
-                <div className="container">
-                    <ShipList />
-                </div>
-            </main>
-            <footer>
-                <Footer />
-            </footer>
-        </>
+        <Router>
+            <div className="layout">
+                <header>
+                    <Nav />
+                </header>
+                <main>
+                    <div className="container">
+                        <Switch>
+                            <Route exact path="/">
+                                <Ships />
+                            </Route>
+                            <Route path="/rockets">
+                                <Rockets />
+                            </Route>
+                        </Switch>
+                    </div>
+                </main>
+                <footer>
+                    <Footer />
+                </footer>
+            </div>
+        </Router>
     );
 }
 
