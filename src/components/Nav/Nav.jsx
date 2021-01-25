440b2ce7c5956b05a07b79696cd835a1d79fd5e3
Anna <anna.stampferova@urbanstore.cz>
2021-01-25 20:24:25 +0100

    navigation

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import { ReactComponent as Logo } from './rocket.svg';
 import styles from './Nav.module.scss';
 
@@ -9,8 +11,25 @@ export const Nav = ({ children }) => {
                 <div className={styles.content}>
                     <div className={styles.logo}>
                         <Logo className={styles.icon} />
+                        <h1 className={styles.title}>SpaceX fleet</h1>
                     </div>
-                    <h1 className={styles.title}>SpaceX fleet</h1>
+                    <div className={styles.links}>
+                        <NavLink
+                            exact={true}
+                            className={styles.link}
+                            to="/"
+                            activeClassName={styles.is_active}
+                        >
+                            Ships
+                        </NavLink>
+                        <NavLink
+                            className={styles.link}
+                            to="/rockets"
+                            activeClassName={styles.is_active}
+                        >
+                            Rockets
+                        </NavLink>
+                    </div>
                 </div>
             </div>
         </nav>
